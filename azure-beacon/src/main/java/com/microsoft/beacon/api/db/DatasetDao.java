db3c3c4aa3367aa65d5814dca3df6cf579cc263f
Yulia Dubinina <yuliadub@users.noreply.github.com>
2024-09-17 11:05:10 -0700

    Initial set up for dataset model (#18)

---
+++ azure-beacon/src/main/java/com/microsoft/beacon/api/db/DatasetDao.java
@@ -0,0 +1,50 @@
+package com.microsoft.beacon.api.db;
+
+import com.microsoft.beacon.api.db.model.Dataset;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Slf4j
+public class DatasetDao {
+  private String SELECT_DATASET_QUERY = "select * from dataset where id = :id";
+  // no paging at this time.
+  private String LIST_DATASET_QUERY = "select * from dataset";
+
+  private static final RowMapper<Dataset> datasetRowMapper =
+      (rs, num) -> new Dataset(rs.getString("id"), rs.getString("name"));
+
+  private final NamedParameterJdbcTemplate jdbcTemplate;
+
+  public DatasetDao(NamedParameterJdbcTemplate jdbcTemplate) {
+    this.jdbcTemplate = jdbcTemplate;
+  }
+
+  public Optional<Dataset> getDataset(String id) {
+    try {
+      Dataset result =
+          jdbcTemplate.queryForObject(SELECT_DATASET_QUERY, Map.of("id", id), datasetRowMapper);
+      return Optional.ofNullable(result);
+    } catch (DataAccessException e) {
+      log.error("Error getting dataset data with id={}. Error: {}", id, e.getMessage());
+      throw e;
+    }
+  }
+
+  public List<Dataset> listDatasets() {
+    List<Dataset> result = Collections.emptyList();
+    try {
+      result = jdbcTemplate.query(LIST_DATASET_QUERY, datasetRowMapper);
+    } catch (DataAccessException e) {
+      log.error("Error getting list of dataset data. Error: {}", e.getMessage());
+    }
+    return result;
+  }
+}
---
+++ azure-beacon/src/main/java/com/microsoft/beacon/api/db/model/Dataset.java
@@ -0,0 +1,76 @@
+package com.microsoft.beacon.api.db.model;
+
+import java.time.LocalDateTime;
+
+public class Dataset {
+  public String Id;
+  public String Name;
+  public String Description;
+  public String Version;
+  public String Info;
+  public LocalDateTime CreateDateTime;
+  public LocalDateTime UpdateDateTime;
+
+  public Dataset(String id, String name) {
+    this.Id = id;
+    this.Name = name;
+    this.CreateDateTime = LocalDateTime.now();
+    this.UpdateDateTime = LocalDateTime.now();
+  }
+
+  public String getId() {
+    return this.Id;
+  }
+
+  public void setId(String id) {
+    this.Id = id;
+  }
+
+  public String getName() {
+    return this.Name;
+  }
+
+  public void setName(String name) {
+    this.Name = name;
+  }
+
+  public String getDescription() {
+    return this.Description;
+  }
+
+  public void setDescription(String description) {
+    this.Description = description;
+  }
+
+  public String getVersion() {
+    return this.Version;
+  }
+
+  public void setVersion(String version) {
+    this.Version = version;
+  }
+
+  public String getInfo() {
+    return this.Info;
+  }
+
+  public void setInfo(String info) {
+    this.Info = info;
+  }
+
+  public LocalDateTime getCreateDateTime() {
+    return this.CreateDateTime;
+  }
+
+  public void setCreateDateTime(LocalDateTime createDateTime) {
+    this.CreateDateTime = createDateTime;
+  }
+
+  public LocalDateTime getUpdateDateTime() {
+    return this.UpdateDateTime;
+  }
+
+  public void setUpdateDateTime(LocalDateTime updateDateTime) {
+    this.UpdateDateTime = updateDateTime;
+  }
+}
